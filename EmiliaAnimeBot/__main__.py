a45f9b8d9009789c459001ac9edac5445efb5c1f
Nine <89542501+Ninematsuno@users.noreply.github.com>
2021-10-09 19:09:35 +0530

    Update __main__.py

--- EmiliaAnimeBot/__main__.py
+++ EmiliaAnimeBot/__main__.py
@@ -87,7 +87,7 @@ Main Commands:
 ❀ /help: PM's you this message.
 ❀ /start: You have already used it.
 All commands can either be used with / or.
-[.] (https://telegra.ph/file/53f8e4acf7c0e72e798e5.jpg) parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=False on
+[.] (https://telegra.ph/file/53f8e4acf7c0e72e798e5.jpg)
 """
 
 IMPORTED = {}
