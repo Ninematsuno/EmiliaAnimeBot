02387601ee39dc8d97aa55152b7637024d9d1121
Nine <89542501+Ninematsuno@users.noreply.github.com>
2021-10-10 20:25:58 +0530

    Update userinfo.py

--- EmiliaAnimeBot/modules/userinfo.py
+++ EmiliaAnimeBot/modules/userinfo.py
@@ -224,7 +224,7 @@ def info(update: Update, context: CallbackContext):
         return
 
     rep = message.reply_text(
-        "<code>Gathering Info...</code>", parse_mode=ParseMode.HTML)
+        "<code>Connecting to minato...</code>", parse_mode=ParseMode.HTML)
 
     text = (f"╒═══「<b> Information Gathered:</b> 」\n"
             f"ID: <code>{user.id}</code>\n"
