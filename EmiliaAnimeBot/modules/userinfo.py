83d70f11172531a7b987e9b8df1ba23fb640bc78
Nine <89542501+Ninematsuno@users.noreply.github.com>
2021-10-11 09:14:03 +0530

    Update userinfo.py

--- EmiliaAnimeBot/modules/userinfo.py
+++ EmiliaAnimeBot/modules/userinfo.py
@@ -293,7 +293,7 @@ if disaster_level_present:
         text += ' [<a href="https://t.me/minato_updates/6">?</a>]'.format(
             bot.username)
 
-    try:
+    
         user_member = chat.get_member(user.id)
         if user_member.status == 'administrator':
             result = requests.post(
